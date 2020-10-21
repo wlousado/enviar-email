a0e6e3787b621cee448a0f3ce448a13e3193947f
William Lima <william.limagm@hotmail.com>
2020-10-21 19:12:01 +0000

    init

--- README.md
+++ README.md
@@ -1,19 +1,14 @@
 # Automação: Enviar e-mails
 
-Com a chegada de novas plataformas para captação de receita, surgiu a necessidade de notificar as filiais com seus respectivos login e senha para acesso para captar a receita nas mesmas. Entretanto cada login e senha são únicos e destinado apenas a filial respectiva, sendo assim, fazendo necessário enviar e-mails individuais para cada filial, respeitando e cumprindo com o sigilo e normas da segurança da informação.
-
 ## Solução
 Foi desenvolvido por mim, um script na linguagem Python para a automação destes envios, poupando o tempo de qualquer responsável por esta função, o script faz a leitura dos dados em um arquivo de extensão CSV, monta o corpo do e-mail, e utiliza de um servidor smtp local para a propagação envio em massa.
 
 ### Requisitos técnicos:
 - Python >= 3.8.3
+- Servidor SMTP
 ### Estrutura do projeto:
 - data/
-	-	indisponibilidade.csv
-	-	memed.csv
-	-	nexodata.csv
-- view/
-	- html.py
+	-	data_example.csv
  - main.py
 
 ### data/
@@ -24,45 +19,19 @@ Nesta pasta é armazenado em arquivos CSV os dados necessários que cada e-mail 
 > 
  Citação retirada de: https://ceweb.br/guias/dados-abertos/capitulo-35/#:~:text=Os%20arquivos%20CSV%20(do%20ingl%C3%AAs,e%20texto)%20em%20texto%20simples.&text=Arquivos%20CSV%20s%C3%A3o%20simples%20e,que%20lidam%20com%20dados%20estruturados.
 
-### view/
-Nesta pasta é armazenado scripts para gerar o corpo de e-mail, lembrando que podemos criar um corpo de e-mail utilizando texto simples, ou notação HTML com CSS Inline.
-
- - html.py
-	 - Separei a responsabilidade de gerar e-mails por funções, cada função gera o padrão que será enviado a 		loja. Para isso, as funções recebem parâmetros, estes parâmetros serão utilizando para gerar o e-mail e por fim, a função retorna o conteúdo do e-mail atualizado com as informações que recebeu por parâmetro
-
 ### main.py
 Esse é o script principal, nele é feito a leitura do arquivo, a geração do e-mail e o envio. Também separei por funções a responsabilidade de envio para cada plataforma. 
 No final deste script, é necessário chamar a função declarada para que seja disparado os e-mails.
 
 Exemplo:
 
-1 - Cadastro e solicitação do Apoio Farmacêutico:
+1 - Popular o arquivo CSV da plataforma (data_example.csv)
+obs: nesta estrutura separei por |E-mail|Usuario|senha
 
-![Exemplo1](http://gitlab.dimed.com.br/wlousado/enviaemail/-/raw/master/screenshot/ex1.png)
+2 - É preciso chamar a função main para que seja invocada e faça o seu papel.
 
-2 - Popular o arquivo CSV da plataforma (nexodata.csv)
-obs: nesta estrutura separei por |Nome da farmácia|Usuario/email|senha
-
-![Exemplo2](http://gitlab.dimed.com.br/wlousado/enviaemail/-/raw/master/screenshot/ex2.png)
-
-3 - É preciso chamar a função responsável para que seja invocada e faça o seu papel,
-então após nos certificarmos que está tudo OK, e na ultima linha chamamos a função.
-![Exemplo3](http://gitlab.dimed.com.br/wlousado/enviaemail/-/raw/master/screenshot/ex3.png)
-
-4 - Ultimo passo é executar o script.
-
-obs: Foi possível chamar o interpretador via linha de comando, pois o mesmo
-se encontra no Path (variável de ambiente do windows), mas é possível utilizar
-qualquer chamada de interpretador desde que execute na versão 3.8.3 ou maior.
-
-![Exemplo4](http://gitlab.dimed.com.br/wlousado/enviaemail/-/raw/master/screenshot/ex4.png)
-
-
-5 - Resultado final:
+3 - Resultado final:
 ![Exemplo5](http://gitlab.dimed.com.br/wlousado/enviaemail/-/raw/master/screenshot/ex5.png)
-
-
-
 
 
 Dúvidas estou a disposição!
--- main.py
+++ main.py
@@ -4,15 +4,14 @@ import csv
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
-from view.html import Nexodata, Memed, Indisponibilidade
 
-def memed():
+def main():
     #Leitura do arquivo CSV
-    with open('data/memed.csv') as csvfile: 
+    with open('data/data_example.csv') as csvfile: 
         readCSV = csv.reader(csvfile, delimiter=';')
         for row in readCSV:
             #A cada iteração do laço (leitura linha a linha), pegamos os dados da linha, e armazenamos em uma variável
-            panvel = row[0]
+            email = row[0]
             usuario = row[1]
             senha = row[2]
 
@@ -24,103 +23,28 @@ def memed():
             msg = MIMEMultipart()
             
             #define os destniatários que receberão o e-mail, pode separar por virgula ex: [destinatario1, destinatario2]
-            destinatarios = [usuario]
+            destinatarios = [email]
             #define quem estará em copia no momento do envio. pode separar por virgula ex: [copia1, copia2]
-            cc = ["suportcs@dimed.com.br"]
+            cc = ["pessoa1@exemplo.com.br"]
 
-            msg['From'] = "Memed Acesso <suportcs@dimed.com.br>"
-            msg['To'] = ", ".join(destinatario)
-            msg['Cc'] = ", ".join(cc)
-            destinatarios.extend(cc)
-
-            msg['Subject'] = "[Memed] Acesso a Plataforma - {0}".format(panvel)
-
-            #aqui é chamado a função para criar o e-mail utilizando os parametros lidos do arquivo csv
-            msg.attach(MIMEText(Memed(panvel, usuario, senha), 'html', 'utf-8'))
-
-            #aqui é enviado o e-mail para todos os destinatários
-            server = smtplib.SMTP('smtp.dimed.local', 25)
-            server.sendmail(msg['From'], destinatarios, msg.as_string())
-            print("{0} - Enviado - {1}".format(panvel, hora))
-            server.quit()
-
-
-def nexodata():
-    with open('data/nexodata.csv') as csvfile:
-        readCSV = csv.reader(csvfile, delimiter=';')
-        for row in readCSV:
-            panvel = row[0]
-            usuario = row[1]
-            senha = row[2]
-
-            msg = MIMEMultipart()
-
-            hora = datetime.datetime.now()
-            hora = hora.strftime("%H:%M")
-
-            destinatarios = [usuario]
-            cc = ["suportcs@dimed.com.br"]
-
-            msg['From'] = "Nexodata Acesso <suportcs@dimed.com.br>"
+            msg['From'] = "Nome Fulano <pessoa@exemplo.com.br>"
             msg['To'] = ", ".join(destinatarios)
             msg['Cc'] = ", ".join(cc)
             destinatarios.extend(cc)
 
-            msg['Subject'] = "[Nexodata] Acesso a Plataforma - {0}".format(panvel)
+            msg['Subject'] = "Assunto do e-mail"
 
-            msg.attach(MIMEText(Nexodata(panvel, usuario, senha), 'html', 'utf-8'))
+            #aqui é se cria o corpo do e-mail
+            msg.attach(MIMEText("""
+                <h1>Olá!</h1>
+                Seu usuário é <strong> {1} </strong> <br>
+                sua senha é <strong> {2} </strong>
+            """.format(usuario, senha), 
+            'html', 'utf-8'))
 
-            server = smtplib.SMTP('smtp.dimed.local', 25)
+            #aqui é enviado o e-mail para todos os destinatários
+            server = smtplib.SMTP('smtp.example.local', 25)
             server.sendmail(msg['From'], destinatarios, msg.as_string())
-
-            print("{0} - Enviado {1}".format(panvel, hora))
+            print("{0} - Enviado - {1}".format(email, hora))
             server.quit()
-
-nexodata()
-
-def indisponibilidade():
-    with open('data/Indisponibilidade.csv') as csvfile:
-        readCSV = csv.reader(csvfile, delimiter=';')
-        for row in readCSV:
-            panvel = row[0]
-            msg = MIMEMultipart()
-            
-            destinatarios = [panvel, 
-            "roquee@panvel.com.br",
-            "fesoares@panvel.com.br",
-            "clsoares@panvel.com.br",
-            "adrodrigues@panvel.com.br",
-            "amviana@panvel.com.br",
-            "azevedof@panvel.com.br",
-            "wposada@panvel.com.br",
-            "jfdias@panvel.com.br",
-            "jmay@panvel.com.br",
-            "lpadilha@panvel.com.br",
-            "rluna@panvel.com.br",
-            "jmoraes@panvel.com.br",
-            "scorrea@panvel.com.br",
-            "jjrodrigues@panvel.com",
-            "crcarvalho@panvel.com.br",
-            "jrigo@panvel.com.br",
-            "lalves@grupodimed.com.br",
-            "suvieira@panvel.com.br",
-            "fonofre@panvel.com.br",
-            "jtoliveira@panvel.com.br",
-            "sampaior@panvel.com.br"
-            ]
-
-            cc = ["eodil@dimed.com.br", "ppastore@dimed.com.br", "bgadea@dimed.com.br"]
-
-            msg["From"] = "Central de Serviços <suportcs@dimed.com.br>"
-            msg["To"] = ", ".join(para)
-            msg['Cc'] = ", ".join(cc)
-            msg['Subject'] = "<ATENÇÃO!> Indisponibilidade nos Serviços de Venda no Final de Semana"
-
-            msg.attach(MIMEText(Indisponibilidade(), 'html', 'utf-8'))
-            para.extend(cc)
-
-            server = smtplib.SMTP('smtp.dimed.local', 25)
-            server.sendmail(msg['From'], destinatarios, msg.as_string())
-            print("{0} - Enviado {1}".format(i, panvel))
-            
-            server.quit()
+main()
